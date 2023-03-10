cb7546cde1d7d7cf9b87989eba2f7df0d6af286f
Skuld Shao <77602197+skuldshao@users.noreply.github.com>
2023-03-10 13:33:34 -0500

    a7 redux

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -3,10 +3,20 @@ import NavigationSidebar
     from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list/index.js";
 import ExploreComponent from "./explore";
+import whoReducer
+    from "./reducers/who-reducer";
+import tuitsReducer from "./tuits/tuits-reducer";
+import { configureStore }
+    from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+const store = configureStore(
+    {reducer: {who: whoReducer, tuits: tuitsReducer}});
+
 function Tuiter() {
     return (
         <div>
-            <Nav/>
+        <Provider store={store}>
+        <Nav/>
         <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
                 <NavigationSidebar active="explore"/>
@@ -19,7 +29,9 @@ function Tuiter() {
                 <WhoToFollowList/>
             </div>
         </div>
+        </Provider>
         </div>
+
     );
 
 }
