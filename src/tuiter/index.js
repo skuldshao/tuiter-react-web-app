da8b190c508e0d5a2a2686f6fef0370cc832e28e
Skuld Shao <77602197+skuldshao@users.noreply.github.com>
2023-04-03 00:36:21 -0400

    fix bugs

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -28,7 +28,7 @@ function Tuiter() {
                  style={{"position": "relative"}}>
                 <Routes>
                     <Route index element={<HomeComponent />} />
-                    <Route path='/explore' element={<ExploreComponent />} />
+                    <Route path='explore' element={<ExploreComponent />} />
                 </Routes>
             </div>
             <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
