faa809c7b20aacb52850918a5434c8af1aa35f2c
Skuld Shao <77602197+skuldshao@users.noreply.github.com>
2023-04-03 00:28:27 -0400

    fix bugs

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const TUITS_API = 'https://tuiter-node-server-app-uygi.onrender.com/';
+const TUITS_API = 'https://tuiter-node-server-app-uygi.onrender.com//api/tuits';
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
